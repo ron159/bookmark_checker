0baee89a3bef058a6442e6ea8f2102f3d25c0a26
ron159 <zlronron159@gmail.com>
2018-12-16 18:20:21 +0800

    引入了多线程，加快访问速度，修改了代码逻辑

--- bookmark_checker.py
+++ bookmark_checker.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests
+from multiprocessing.dummy import Pool
 
 localPath="C:\\Users\\young\\Desktop\\bookmarks_2018_12_15.html"
 
@@ -10,22 +11,22 @@ def getUrls(path):
         urls.append(link.get('href'))
     return urls
 
+badLinks=[]
 def verifyUrl(links):
-    badLinks=[]
-    for i in links:
-        try:
-            r=requests.get(i,timeout=10)
-            print("{}:OK".format(i))
-        except requests.exceptions.ConnectionError:
-            print("{}:连接错误".format(i))
-            badLinks.append(i)
-        except:
-            print("{}:其他错误".format(i))
-            badLinks.append(i)
-    print("以下是所有坏链，请手动验证：")
-    print(badLinks)
-
+#    for i in links:
+    try:
+        requests.get(links,timeout=10)
+        print("{}:OK".format(links))
+    except requests.exceptions.ConnectionError:
+        print("{}:连接错误".format(links))
+        badLinks.append(links)
+    except:
+        print("{}:其他错误".format(links))
+        badLinks.append(links)
 
 if __name__ == "__main__":
     p=getUrls(localPath)
-    verifyUrl(p)
+    pool=Pool(processes=20)
+    pool.map(verifyUrl,p)
+    print("以下是所有坏链，请手动验证：")
+    print(badLinks)
