7cc8b2fc8c8860de81bdaa3d17520c67acf096d5
ron159 <zlronron159@gmail.com>
2019-02-22 08:34:22 +0800

    增加输出为excel文件，方便手动坏链检查

--- bookmark_checker.py
+++ bookmark_checker.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup as bs
 import requests
 from multiprocessing.dummy import Pool
+import os
 
-localPath="C:\\Users\\young\\Desktop\\bookmarks_2018_12_20.html"#修改路径
+localPath="C:\\Users\\young\\Desktop\\bookmarks_2019_2_22.html"#修改路径
 
 def getUrls(path):
     urls=[]
@@ -28,5 +29,14 @@ if __name__ == "__main__":
     p=getUrls(localPath)
     pool=Pool(processes=20)
     pool.map(verifyUrl,p)
-    print("以下是所有坏链，请手动验证：")
-    print(badLinks)
+#    print("以下是所有坏链，请手动验证：")
+#    print(badLinks)
+    output = open('data.xls','w',encoding='gbk')#输出为excel格式，路径请自行替换
+    output.write('Links\n')
+    for i in range(len(badLinks)):
+        output.write(badLinks[i])    
+        output.write('\t')   #Tab一下，换一个单元格
+        output.write('\n')
+    output.close()
+
+
